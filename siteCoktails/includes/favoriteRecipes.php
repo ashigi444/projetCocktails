2997717cbf038d1c7d428eaee173ebb4bcd43d3b
Dorian_Pareja <dorian.pareja9@etu.univ-lorraine.fr>
2025-11-21 12:24:14 +0100

    Editing the favorite recipes page

--- siteCoktails/includes/favoriteRecipes.php
+++ siteCoktails/includes/favoriteRecipes.php
@@ -1,2 +1,8 @@
 <h2>Recettes pr&eacute;f&eacute;r&eacute;es</h2>
-<p>ici on affichera les recettes favorites.</p>
+
+<?php if (isset($user) && isset($user['username'])) { ?>
+    <!-- TODO (traitement de l'affichage des recettes favorites) -->
+    <p>ici on affichera les recettes favorites.</p>
+<?php } else { ?>
+    <p>Veuillez vous connecter ou cr&eacute;er un compte pour acc&eacute;der aux recettes favorites.</p>
+<?php } ?>
